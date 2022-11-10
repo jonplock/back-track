4b795ce4083bc35962a9a8e980a5d8bc9b94b8ae
Jon Boman <jonboman@kth.se>
2022-11-10 21:35:23 +0100

    New functionality Added subtraction :o

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def add (a, b):
     return a+b
 
+def subtract(e, f):
+    return e-f
+
 def main(c, d):
     print(add(c, d))
+    print(subtract(c, d))
 
 if __name__ == '__main__':
     main(6, 7)
